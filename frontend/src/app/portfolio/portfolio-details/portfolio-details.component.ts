b490208dcf4cea945295bd1d9db4fce7368f3610
nabil.alhafez <nabil.alhfz98@gmail.com>
2023-11-30 22:46:46 +0200

    # Task - Portfolio details: - Creates a basic portfolio details component.

---
+++ frontend/src/app/portfolio/portfolio-details/portfolio-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-portfolio-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './portfolio-details.component.html',
+  styleUrl: './portfolio-details.component.css'
+})
+export class PortfolioDetailsComponent {
+
+}
